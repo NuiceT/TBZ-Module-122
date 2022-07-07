079a78f07b7fe370962fa2e0be9150faa70b60bd
Leon Babic <leon@leonbabic.ch>
2022-07-07 21:54:53 +0200

    ✨ Add Logger

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from distutils.log import error
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import logging
 import os
 import string
 import requests
@@ -11,12 +12,16 @@ import smtplib as mail
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
+logging.basicConfig(level=logging.INFO)
+
 
 def sendMail(subject: string, msg: string, attachment: string):
     try:
         smtp = mail.SMTP_SSL(host=os.getenv("SMTP_HOST"), port=os.getenv("SMTP_PORT"))
         smtp.ehlo()
         smtp.login(os.getenv("SMTP_USERNAME"), os.getenv("SMTP_PASSWORD"))
+
+        logging.info("Logged in with USERNAME: " + os.getenv("SMTP_USERNAME"))
 
         message = MIMEMultipart("mixed")
         message["From"] = "Automatic Bot <{}>".format(os.getenv("MAIL_SENDER"))
@@ -26,19 +31,24 @@ def sendMail(subject: string, msg: string, attachment: string):
         body = MIMEText(msg, "html")
         message.attach(body)
 
+        logging.info("Attached body")
+
         with open(attachment, "rb") as f:
             # attach = email.mime.application.MIMEApplication(f.read(),_subtype="pdf")
             attach = MIMEApplication(f.read(), _subtype="pdf")
         attach.add_header("Content-Disposition", "attachment", filename=attachment)
         message.attach(attach)
 
+        logging.info("Attached attachment")
+
         smtp.sendmail(
             os.getenv("MAIL_SENDER"), os.getenv("MAIL_RECIPIENT"), message.as_string()
         )
-        print("Successfully sent E-Mail")
+
+        logging.info("Sent E-Mail to " + os.getenv("MAIL_RECIPIENT"))
         smtp.quit()
     except mail.SMTPException:
-        print("Error: Unable to send E-Mail")
+        logging.error("Error: Unable to send E-Mail")
 
 
 def createFigure(dates: array, prices: int):
@@ -46,7 +56,9 @@ def createFigure(dates: array, prices: int):
     plt.plot(dates, prices)
     plt.xlabel("Date")
     plt.ylabel("Price per unit in CHF")
+    logging.info("Created figure")
     plt.savefig("report_" + dates[7] + ".pdf")
+    logging.info("Saved figure")
 
 
 def main():
@@ -73,6 +85,7 @@ def main():
             )
             + "</td></tr>"
         )
+    logging.info("Retrieved data")
     createFigure(dates, prices)
     sendMail(
         "Wöchentlicher Bitcoin Bericht",
